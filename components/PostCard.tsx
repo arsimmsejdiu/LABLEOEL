5711853c441580f70f2505da05dce26e340ce7db
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-05-17 22:49:57 +0200

    Building Clients Blog Application

---
+++ components/PostCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface PostCardProps {
+  title: string
+  excert: string
+}
+
+const PostCard = ({ title, excert }: PostCardProps) => {
+  return (
+    <div>
+      {title}
+      {excert}
+    </div>
+  )
+}
+
+export default PostCard
