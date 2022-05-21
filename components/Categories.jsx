d725208f5a8e3ee9940bc9f6f71883b478cb4b29
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-05-22 01:55:27 +0200

    Added the Categories in the header

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,27 +1,33 @@
-import React, {useState, useEffect} from 'react'
-import Link from 'next/link';
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
 
-import { getCategories } from '../services';
+import { getCategories } from '../services'
 
 const Categories = () => {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([])
 
   useEffect(() => {
     getCategories().then((newCategories) => {
-      setCategories(newCategories);
-    });
-  }, []);
+      setCategories(newCategories)
+    })
+  }, [])
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
-          <span className={`cursor-pointer block ${(index === categories.length - 1) ? 'border-b-0' : 'border-b'} pb-3 mb-3`}>{category.name}</span>
+          <span
+            className={`block cursor-pointer ${
+              index === categories.length - 1 ? 'border-b-0' : 'border-b'
+            } mb-3 pb-3`}
+          >
+            {category.name}
+          </span>
         </Link>
       ))}
     </div>
-  );
+  )
 }
 
 export default Categories
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,27 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Link from 'next/link'
-import { HiMenuAlt4 } from "react-icons/hi"
-
-const categories = [
-  {
-    name: 'Azure DevOps',
-    slug: 'azure-devops',
-  },
-  {
-    name: 'Microsoft .NET',
-    slug: 'microsoft-dotnet',
-  },
-  {
-    name: 'SQL Server',
-    slug: 'sql-server',
-  },
-  {
-    name: 'Azure Functions',
-    slug: 'azure-functions',
-  },
-]
+import { HiMenuAlt4 } from 'react-icons/hi'
+import { getCategories } from '../services'
 
 const Header = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories)
+    })
+  }, [])
+
   return (
     <div className="container mx-auto mb-8 px-5">
       <div className="inline-block w-full border-b border-blue-400 py-5">
@@ -33,17 +23,19 @@ const Header = () => {
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-          {categories.map(({ name, slug }) => (
-            <Link key={slug} href={`/category/${slug}`}>
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}>
               <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
-                {name}
+                {category.name}
               </span>
             </Link>
           ))}
-          <HiMenuAlt4
-            fontSize={28}
-            className="text-white md:hidden cursor-pointer"
-          />
+          {/* <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
+            <HiMenuAlt4
+              fontSize={28}
+              className="cursor-pointer text-white md:hidden"
+            />
+          </span> */}
         </div>
       </div>
     </div>
