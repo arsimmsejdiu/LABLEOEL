fca2e6be45beb78f3cc1e5f5811def1f1c1abd65
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-05-23 01:07:12 +0200

    Bugs fixing

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -11,6 +11,7 @@ import {
 import { AdjacentPosts } from '../../sections';
 
 const PostDetails = ({ post }) => {
+  console.log(post)
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
@@ -39,10 +40,10 @@ export default PostDetails
 
 // Fetch data at build time
 export async function getStaticProps({ params }) {
-  const data = await getPostDetails(params.slug);
+  const post= await getPostDetails(params.slug);
   return {
     props: {
-      post: data,
+      post,
     },
   };
 }
