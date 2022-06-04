7322ec072a9644b5bb5fa5b07a46ce8fa842bc85
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-06-04 23:14:25 +0200

    Added Bubble Animation to the app

---
+++ components/Circle.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Circle = () => {
+  return (
+    <ul className="circles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  )
+}
+
+export default Circle
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react'
 import '../styles/globals.scss'
 import { ThemeProvider } from 'next-themes';
 import { Layout } from '../components';
+import Circle from '../components/Circle';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
       <Layout>
+        <Circle />
         <Component {...pageProps} />
       </Layout>
     </ThemeProvider>
